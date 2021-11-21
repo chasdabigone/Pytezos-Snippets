fa57e19a134b124aa7b3dd5ae809ecd877885af8
chas s <chasdabigone@gmail.com>
2021-11-21 15:18:06 -0600

    Update fetch_token_metadata_FA2.py

--- fetch_token_metadata_FA2.py
+++ fetch_token_metadata_FA2.py
@@ -7,5 +7,5 @@ token = token.contract('TOKEN_ADDRESS')
 token_id = 'TOKEN_ID'
 
 token_metadata = list(token.storage['token_metadata'][token_id]()['token_info'].values())[0].decode()
-pair1_metadata = pair1_metadata.replace('ipfs://','https://ipfs.io/ipfs/')
-pair1_metadata = requests.get(pair1_metadata).json()
+token_metadata = token_metadata.replace('ipfs://','https://ipfs.io/ipfs/')
+token_metadata = requests.get(token_metadata).json()
